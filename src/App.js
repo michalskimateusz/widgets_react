f6be8bd874ed42b5e056418476097c9dda05a93f
Mateusz Michalski <michalskimateusz@outlook.com>
2021-07-22 14:01:57 +0200

    navigation bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -34,21 +36,33 @@ const options = [
   },
 ];
 
+
+
+
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
 
 
   return (
     <div className="ui container">
-      {/* <Accordion items={items}/> */}
-      {/* <Search/> */}
-      {/* <Dropdown
-        label={'Choose a color'}
-        onSelectedChange={setSelected}
-        selected={selected}
-        options={options}
-      /> */}
-      <Translate />
+      <Header/>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search/>
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label={'Choose a color'}
+          onSelectedChange={setSelected}
+          selected={selected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate/>
+      </Route>
     </div>
   )
 };
